40854d223bf2db3b1b6bc23ad00a8d09ab0faf7a
Gregory Casamento <greg.casamento@gmail.com>
2024-04-21 06:58:57 -0400

    Initial Commit

---
+++ NSOutlineCellView_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSOutlineCellView_test
+//
+//  Created by Gregory John Casamento on 4/21/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSOutlineCellView_test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSOutlineCellView_test
+//
+//  Created by Gregory John Casamento on 4/21/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
